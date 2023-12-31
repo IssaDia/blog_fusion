0e5037a6ed256a2d2ca7ee0098ab025564681ad0
IssaDia <44264590+IssaDia@users.noreply.github.com>
2023-12-31 19:10:25 +0100

    added missing dependency

--- app/components/homepage/Homepage.jsx
+++ app/components/homepage/Homepage.jsx
@@ -24,7 +24,7 @@ export default function Homepage() {
     };
 
     fetchArticles();
-  }, []);
+  }, [visitedArticles]);
 
   const markArticleAsVisited = (articleId, event) => {
     setVisitedArticles((prevVisited) => ({
